b1b126b355c1bf2d427ae9ad95c4d2ac482b6d49
iriantogalih <iriantogalih1@gmail.com>
2024-12-03 13:56:33 +0700

    change source data event from temp to schema, setup pagination and search button

--- app/(dashboard)/list/events/page.tsx
+++ app/(dashboard)/list/events/page.tsx
@@ -3,20 +3,14 @@ import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import Tablesearch from "@/components/Tablesearch"
 import { eventsData, role } from "@/lib/data"
+import prisma from "@/lib/prisma"
+import { ITEM_PER_PAGE } from "@/lib/settings"
+import { Class, Event, Prisma } from "@prisma/client"
 import Image from "next/image"
 import Link from "next/link"
 import { ImageResponse } from "next/server"
 
-{/* temporary load data */}
-
-type Event = {
-  id: number,
-  title: string,
-  class: string,
-  date: string,
-  startTime: string,
-  endTime: string,
-}
+type Eventlist = Event & {class: Class}
 
 {/* Create header table */}
 const columns = [
@@ -30,7 +24,7 @@ const columns = [
     accessor: "class",
   },
   {
-    header: "Date", 
+    header: "Description", 
     accessor: "date", 
     className:"hidden lg:table-cell",
   },
@@ -50,37 +44,84 @@ const columns = [
   },
 ]
 
-const EventsListPage = () => {
+const renderRow = (item: Eventlist) => (
+  <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
+    <td className="">{item.title}</td>
+    <td className="">{item.class.name}</td>
+    <td className="hidden lg:table-cell">{item.description}</td>
+    <td className="hidden lg:table-cell">{new Intl.DateTimeFormat("id-ID", {dateStyle:"medium"}).format(item.startDate)}</td>
+    <td className="hidden lg:table-cell">{new Intl.DateTimeFormat("id-ID", {dateStyle:"medium"}).format(item.endDate)}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        {/*<Link href={`/list/events/${item.id}`}>
+          <button className="w-7 h-7 flex items-center justify-center bg-lamaSky rounded-full">
+            <Image src="/edit.png" alt="" width={16} height={16} />
+          </button>
+          
+        </Link>*/}
+        {role === "admin" && (
+          //<button className="w-7 h-7 flex items-center justify-center bg-lamaPurple rounded-full">
+          //  <Image src="/delete.png" alt="" width={16} height={16} />
+          //</button>
+          <>
+            <Formmodal table="event" type="update" data={item} />
+            <Formmodal table="event" type="delete" id={item.id} />
+          </>
+          
+        )}
+      </div>
+    </td>
+  </tr>
+)
 
-  const renderRow = (item: Event) => (
-    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
-      <td className="">{item.title}</td>
-      <td className="">{item.class}</td>
-      <td className="hidden lg:table-cell">{item.date}</td>
-      <td className="hidden lg:table-cell">{item.startTime}</td>
-      <td className="hidden lg:table-cell">{item.endTime}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {/*<Link href={`/list/events/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center bg-lamaSky rounded-full">
-              <Image src="/edit.png" alt="" width={16} height={16} />
-            </button>
-            
-          </Link>*/}
-          {role === "admin" && (
-            //<button className="w-7 h-7 flex items-center justify-center bg-lamaPurple rounded-full">
-            //  <Image src="/delete.png" alt="" width={16} height={16} />
-            //</button>
-            <>
-              <Formmodal table="event" type="update" data={item} />
-              <Formmodal table="event" type="delete" id={item.id} />
-            </>
-            
-          )}
-        </div>
-      </td>
-    </tr>
-  )
+const EventsListPage = async ({
+  searchParams,
+}:{
+  searchParams: {[key:string]:string | undefined}
+}) => {
+  
+  const {page, ...queryParams} = searchParams;
+
+  const p = page ? parseInt(page) : 1;
+
+  // URL PARAMS CONDITION
+
+  const query: Prisma.EventWhereInput = {}
+
+  if (queryParams) {
+    for (const[key,value] of Object.entries(queryParams)) {
+      if(value !== undefined){
+        switch(key) {
+          case "search":
+            query.OR = [
+              {title: {contains:value, mode:"insensitive"}},
+              {description: {contains:value, mode:"insensitive"}},
+              {class: {name: {contains:value, mode:"insensitive"}}},
+            ]
+
+          default:
+          break
+        }
+      }
+      
+    }
+  }
+
+  const [data, count] = await prisma.$transaction([
+    prisma.event.findMany({
+      where:query,
+      include:{
+        class: {select:{name:true}}
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    prisma.event.count({
+      where:query,
+    })
+  ])
+
+  
 
   return (
     <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0'>
@@ -107,9 +148,9 @@ const EventsListPage = () => {
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={eventsData}/>
+      <Table columns={columns} renderRow={renderRow} data={data}/>
       {/* PAGINATION */}
-      <Pagination />
+      <Pagination page={p} count={count} />
     </div>
   )
 }
