07fc109b96271e0112231083bddf515f3e32f9ed
iriantogalih <iriantogalih1@gmail.com>
2024-12-15 18:39:03 +0700

    add event calendar in page parent and teacher

--- app/(dashboard)/teacher/page.tsx
+++ app/(dashboard)/teacher/page.tsx
@@ -1,7 +1,12 @@
 import Announcement from "@/components/Announcement"
 import Bigcalendar from "@/components/Bigcalendar"
+import EventCalendarContainer from "@/components/EventCalendarContainer"
 
-const TeacherPage = () => {
+const TeacherPage = ({
+  searchParams
+}:{
+  searchParams:{[keys:string]:string | undefined
+  }}) => {
   return (
     <div className=" flex-1 p-4 flex gap-4 flex-col xl:flex-row">
       {/* Left Side */}
@@ -13,7 +18,8 @@ const TeacherPage = () => {
       </div>
       {/* Right Side */}
       <div className="w-full xl:w-1/3 flex flex-col gap-8 ">
-        
+        {/* Calendar */}
+        <EventCalendarContainer searchParams={searchParams} />
         {/* Announcement */}
         <Announcement />
       </div>
