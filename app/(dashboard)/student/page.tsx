6e9f68ee80d8b9a8f64850f82b5a4f85b46ec771
iriantogalih <iriantogalih1@gmail.com>
2024-12-15 18:35:50 +0700

    update fetch data for page announcement and admin page

--- app/(dashboard)/student/page.tsx
+++ app/(dashboard)/student/page.tsx
@@ -1,8 +1,13 @@
 import Announcement from "@/components/Announcement"
 import Bigcalendar from "@/components/Bigcalendar"
+import EventCalendarContainer from "@/components/EventCalendarContainer"
 import Eventcalendar from "@/components/Eventcalendar"
 
-const StudentPage = () => {
+const StudentPage = ({
+  searchParams
+}:{
+  searchParams:{[keys:string]:string | undefined
+  }}) => {
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
       {/* Left Side */}
@@ -15,7 +20,7 @@ const StudentPage = () => {
       {/* Right Side */}
       <div className="w-full xl:w-1/3 flex flex-col gap-8 ">
         {/* Calendar */}
-        <Eventcalendar />
+        <EventCalendarContainer searchParams={searchParams} />
         {/* Announcement */}
         <Announcement />
       </div>
